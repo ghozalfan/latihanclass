4c03d41bcc1924972409a0dd4a735cee726984c2
ghozalfan <faqihghozalfan@students.amikom.ac.id>
2023-04-03 23:25:40 +0700

    first commit

---
+++ TAXI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            taxi taxi1 = new taxi();
+
+            taxi1.Drivername = "Yadi";
+            taxi1.OnDuty = true;
+            taxi1.NumPassenger = 10;
+
+
+            taxi1.TaxiInfo();
+            taxi1.dropofpassenger();
+            taxi1.pickuppassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
